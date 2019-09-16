f6cd88c75e3404252eea51bdf7de5ec6575ef1e0
praveen-me <praveenkrsaini0@gmail.com>
2019-09-16 14:16:13 +0530

    Add filters to filters screen

--- shoply/screens/FiltersScreen.js
+++ shoply/screens/FiltersScreen.js
@@ -1,12 +1,62 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, View, Text, StyleSheet, Switch } from "react-native";
 import MenuButton from "../components/MenuButton";
+import Colors from "../constants/Colors";
+
+const FilterSwitch = ({ label, state, handleValueChange }) => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{label}</Text>
+      <Switch
+        value={state}
+        thumbColor={Colors.primary}
+        onValueChange={handleValueChange}
+        trackColor={{ true: Colors.accent }}
+      />
+    </View>
+  );
+};
 
 const FiltersScreen = () => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
+  const handleGlutenChange = () => setIsGlutenFree(!isGlutenFree);
+
+  const handleLactoseChange = () => setIsLactoseFree(!isLactoseFree);
+
+  const handleVeganChange = () => setIsVegan(!isVegan);
+
+  const handleVegetarianChange = () => setIsVegetarian(!isVegetarian);
+
   return (
-    <View style={styles.screen}>
-      <Text>The Filters Screen !!</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.screen}>
+        <Text style={styles.title}>Available Filters / Restrictions</Text>
+        <FilterSwitch
+          label="Gluten Free"
+          state={isGlutenFree}
+          handleValueChange={handleGlutenChange}
+        />
+        <FilterSwitch
+          label="Lactose Free"
+          state={isLactoseFree}
+          handleValueChange={handleLactoseChange}
+        />
+        <FilterSwitch
+          label="Vegan"
+          state={isVegan}
+          handleValueChange={handleVeganChange}
+        />
+        <FilterSwitch
+          label="Vegetarian"
+          state={isVegetarian}
+          handleValueChange={handleVegetarianChange}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -20,7 +70,19 @@ FiltersScreen.navigationOptions = navOptions => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    marginVertical: 10,
+    fontSize: 18,
+    fontFamily: "robotoBold",
+    textAlign: "center"
+  },
+  filterContainer: {
+    marginVertical: 15,
+    width: "80%",
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center"
   }
 });
