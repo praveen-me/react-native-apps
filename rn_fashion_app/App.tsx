28f5411dbd913d2a4087a179d57e65f4e11a947f
praveen-me <praveenkrsaini0@gmail.com>
2020-07-20 22:47:04 +0530

    Added minimal configuration

---
+++ rn_fashion_app/App.tsx
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Onboarding} from './src/containers/Authentication';
+
+const AuthNavigatorStack = createStackNavigator();
+const AuthNavigator = () => {
+  return (
+    <AuthNavigatorStack.Navigator>
+      <AuthNavigatorStack.Screen name={'Onboarding'} component={Onboarding} />
+    </AuthNavigatorStack.Navigator>
+  );
+};
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer>
+  );
+}
---
+++ rn_fashion_app/src/containers/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Onboarding = () => {
+  return (
+    <View>
+      <Text
+        style={{
+          fontFamily: 'Roboto-Bold',
+        }}>
+        Onboarding
+      </Text>
+    </View>
+  );
+};
+
+export default Onboarding;
