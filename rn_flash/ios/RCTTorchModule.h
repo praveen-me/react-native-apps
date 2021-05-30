7b9f4c04b54e7820a040b4e3b988092cc43a4522
praveen-me <praveenkrsaini0@gmail.com>
2021-05-30 14:19:27 +0530

    Added rn_flash with custom ios bridge

---
+++ rn_flash/ios/RCTTorchModule.h
@@ -0,0 +1,12 @@
+//
+//  RCTTorchModule.h
+//  rn_flash
+//
+//  Created by Praveen on 30/05/21.
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface RCTTorchModule : NSObject <RCTBridgeModule>
+
+@end
